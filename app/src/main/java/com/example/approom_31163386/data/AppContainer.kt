721f7315af1521acc3894f000ae1c2e348f02466
Vinicius <viniciusgcruz@gmail.com>
2024-11-25 06:36:06 -0300

    ui/item/ItemEntryViewModel.kt LOADING ItemUiState

---
+++ app/src/main/java/com/example/approom_31163386/data/AppContainer.kt
@@ -0,0 +1,20 @@
+import android.content.Context
+
+/**
+ * App container for Dependency injection.
+ */
+interface AppContainer {
+    val itemsRepository: ItemsRepository
+}
+
+/**
+ * [AppContainer] implementation that provides instance of [OfflineItemsRepository]
+ */
+class AppDataContainer(private val context: Context) : AppContainer {
+    /**
+     * Implementation for [ItemsRepository]
+     */
+    override val itemsRepository: ItemsRepository by lazy {
+        OfflineItemsRepository()
+    }
+}
